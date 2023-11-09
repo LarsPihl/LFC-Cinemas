935a7988dae24667327b525b3bf15eb5c28a20a1
Cappe <142906101+CappeSun@users.noreply.github.com>
2023-11-09 16:01:07 +0100

    Fixed dbLoad directory + code refit

--- moviepage-draft.php
+++ moviepage-draft.php
@@ -1,6 +1,6 @@
 <?php
 
-require '-stuff/dbLoad.php';
+require 'dbStuff/dbLoad.php';
 
 ?>
 
@@ -14,28 +14,27 @@ require '-stuff/dbLoad.php';
 	<link rel="stylesheet" type="text/css" href="moviepage.css">
 </head>
 <body>
-	<?php for ($i = 0; $i < sizeof($fetchData); $i++){ ?>
-		<div id="page<?= $i; ?>" class="page">
-			<img src="<?= $fetchData[$i]['poster_url']; ?>" class="poster">
-			<div class="sect1">
-				<div class="info">
-					<h1 class="title"><?= $fetchData[$i]['title']; ?></h1>
-					<div class="genInfo">
-						<span class="year"><?= $fetchData[$i]['year']; ?></span>
-						<span class="genre"><?= $fetchData[$i]['genre']; ?></span>
-						<span class="age"><?= $fetchData[$i]['age']; ?>+</span>
-					</div>
-					<h3 class="studio"><?= $fetchData[$i]['studio']; ?></h3>
-					<h4 class="credit">Directed by <?= $fetchData[$i]['director']; ?></h4>
-					<h4 class="credit">Written by <?= $fetchData[$i]['writer']; ?></h4>
-					<h4 class="credit">Soundtrack by <?= $fetchData[$i]['composer']; ?></h4>
+	<?php $i = 0; //page id goes here ?>
+	<div id="page<?= $i; ?>" class="page">
+		<img src="<?= $fetchData[$i]['poster_url']; ?>" class="poster">
+		<div class="sect1">
+			<div class="info">
+				<h1 class="title"><?= $fetchData[$i]['title']; ?></h1>
+				<div class="genInfo">
+					<span class="year"><?= $fetchData[$i]['year']; ?></span>
+					<span class="genre"><?= $fetchData[$i]['genre']; ?></span>
+					<span class="age"><?= $fetchData[$i]['age']; ?>+</span>
 				</div>
-				<video class="trailer" controls>
-					<source src="<?= $fetchData[$i]['trailer_url']; ?>">
-				</video>
+				<h3 class="studio"><?= $fetchData[$i]['studio']; ?></h3>
+				<h4 class="credit">Directed by <?= $fetchData[$i]['director']; ?></h4>
+				<h4 class="credit">Written by <?= $fetchData[$i]['writer']; ?></h4>
+				<h4 class="credit">Soundtrack by <?= $fetchData[$i]['composer']; ?></h4>
 			</div>
-			<p class="desc"><?= $fetchData[$i]['desc']; ?></p>
+			<video class="trailer" controls>
+				<source src="<?= $fetchData[$i]['trailer_url']; ?>">
+			</video>
 		</div>
-	<?php } ?>
+		<p class="desc"><?= $fetchData[$i]['desc']; ?></p>
+	</div>
 </body>
 </html>
